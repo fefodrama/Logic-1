4e9f09ed7b049c5f273b962a5d7187bc1b81b594
fefodrama <50503734+fefodrama@users.noreply.github.com>
2019-05-10 20:33:47 -0300

    Update and rename ConversorBinarioDECIMAL to ConversorBinarioDECIMAL.java

--- ConversorBinarioDECIMAL.java
+++ ConversorBinarioDECIMAL.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class conversorBINDecimal {
+public class ConversorBinarioDECIMAL {
 
 	public static void main(String[] args) throws Exception {
 		
